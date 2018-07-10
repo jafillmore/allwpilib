ba4f7b1ac144bf707abdaae67f61cc04abb7291f
KadenK <kaden.keep@gmail.com>
2018-07-09 17:36:41 -1000

    Functioning Maven upload script and POMs for the GradleRIO dependencies
    Note: These POM files and upload scipt do not contain debug.so files and do not publish to the main Kauailabs Maven Repo, but instead to the kauailabs_test repo.

---
+++ Maven/mavenupload.sh
@@ -0,0 +1,62 @@
+set -x
+
+mkdir ~/maven-up/opencv-jni/linux/mau -p
+cp -R /usr/local/share/OpenCV/java/libopencv_java331.so ~/maven-up/opencv-jni/linux/mau
+pushd ~/maven-up/opencv-jni
+jar cf ../opencv-jni.jar ./
+popd
+
+#******TODO replace with the custom library under same name
+mkdir ~/maven-up/hal/linux/mau/shared -p
+cp -R /usr/local/wpilib/lib/libwpiHal.so ~/maven-up/hal/linux/mau/shared/libwpiHalMau.so
+pushd ~/maven-up/hal
+zip -r ../hal.zip ./
+popd
+
+mkdir ~/maven-up/cscore/linux/mau/shared -p
+cp -R /usr/local/wpilib/lib/libcscore.so ~/maven-up/cscore/linux/mau/shared
+pushd ~/maven-up/cscore
+zip -r ../cscore.zip ./
+popd
+
+mkdir ~/maven-up/ntcore/linux/mau/shared -p
+cp -R /usr/local/wpilib/lib/libntcore.so ~/maven-up/ntcore/linux/mau/shared
+pushd ~/maven-up/ntcore
+zip -r ../ntcore.zip ./
+popd
+
+mkdir ~/maven-up/wpiutil/linux/mau/shared -p
+cp -R /usr/local/wpilib/lib/libwpiutil.so ~/maven-up/wpiutil/linux/mau/shared
+pushd ~/maven-up/wpiutil
+zip -r ../wpiutil.zip ./
+popd
+
+
+mkdir ~/maven-up/opencv-cpp/linux/mau/shared -p
+cp -R /usr/lib/arm-linux-gnueabihf/libopencv_*.* ~/maven-up/opencv-cpp/linux/mau/shared
+cp -R /usr/local/share/OpenCV/java/libopencv_java*.so ~/maven-up/opencv-cpp/linux/mau/shared
+pushd ~/maven-up/opencv-cpp
+zip -r ../opencv-cpp.zip ./
+popd
+
+
+pushd poms/opencv-jni
+sudo mvn deploy:deploy-file -DpomFile=pom.xml -Dfile=/home/pi/maven-up/opencv-jni.jar -DrepositoryId=kauailabs-maven-ftp -Durl=ftp://ftp.kauailabs.com/kauailabs.com/maven2
+popd
+pushd poms/hal
+sudo mvn deploy:deploy-file -DpomFile=pom.xml -Dfile=/home/pi/maven-up/hal.zip -DrepositoryId=kauailabs-maven-ftp -Durl=ftp://ftp.kauailabs.com/kauailabs.com/maven2
+popd
+pushd poms/cscore
+sudo mvn deploy:deploy-file -DpomFile=pom.xml -Dfile=/home/pi/maven-up/cscore.zip -DrepositoryId=kauailabs-maven-ftp -Durl=ftp://ftp.kauailabs.com/kauailabs.com/maven2
+popd
+pushd poms/ntcore
+sudo mvn deploy:deploy-file -DpomFile=pom.xml -Dfile=/home/pi/maven-up/ntcore.zip -DrepositoryId=kauailabs-maven-ftp -Durl=ftp://ftp.kauailabs.com/kauailabs.com/maven2
+popd
+pushd poms/wpiutil
+sudo mvn deploy:deploy-file -DpomFile=pom.xml -Dfile=/home/pi/maven-up/wpiutil.zip -DrepositoryId=kauailabs-maven-ftp -Durl=ftp://ftp.kauailabs.com/kauailabs.com/maven2
+popd
+pushd poms/opencv-cpp
+sudo mvn deploy:deploy-file -DpomFile=pom.xml -Dfile=/home/pi/maven-up/opencv-cpp.zip -DrepositoryId=kauailabs-maven-ftp -Durl=ftp://ftp.kauailabs.com/kauailabs.com/maven2
+popd
+
+rm -r ~/maven-up
