c7b0548c5fa00d64f4ad68ae184c423b4f1bac2f
kauailabs <scott@kauailabs.com>
2020-04-14 14:40:48 -1000

    Added automated staging of vmx-pi hal-cpp build artifacts to oss.sonatype.org

--- Kauailabs/BuildScripts/publish_to_maven_central.sh
+++ Kauailabs/BuildScripts/publish_to_maven_central.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+if [ ! -f "login" ]; then
+  echo 'Login file does not exist'
+  exit
+else
+  LOGIN=$(cat "login")
+fi
+
+if [ ! -f "pass" ]; then
+  echo 'Password file does not exist'
+  exit
+else
+  PASS=$(cat "pass")
+fi
+
 cd ~/.m2/repository/com/kauailabs/vmx/first/hal/hal-cpp
 latest_subdir=$(ls -td -- */ | head -n 1)
 cd $latest_subdir
@@ -7,9 +21,21 @@ ls *
 jar -cvf bundle.jar *.pom *.asc *.zip
 ls *
 
+# Stage artifact bundle to oss.sonatype.org
+
+curl -ujorlina2 -u $LOGIN:$PASS --request POST -F "file=@bundle.jar" "https://oss.sonatype.org/service/local/staging/bundle_upload"
+
 publish_bundle_dir=$(pwd)
-echo "******************************************************"
 echo
-echo "Upload the bundle file ($publish_bundle_dir/bundle.jar) as an Artifact Bundle on the Staging Upload page at the Sonatype OSS site (oss.sonatype.org)"
+echo "*****************  MANUAL STEP REQUIRED  ***************************"
+echo "The VMX-pi WPI HAL artifact bundle (bundle.jar) has been uploaded to"
+echo "https://oss.sonatype.org (to the 'Staging Upload' page, using the "
+echo "'Artifact Bundle' upload mode)."
 echo
-echo "******************************************************"
+echo "This artifact should now be released on the 'Staging Repositories' "
+echo "page, by selecting the artifact and pressing 'Release'."
+echo
+echo "Released artifacts appear in Maven Central approximately 20 minutes"
+echo "after being released."
+echo "*********************************************************************"
+echo
